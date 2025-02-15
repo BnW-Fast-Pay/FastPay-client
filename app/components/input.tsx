4ab84723f9e2cdd7048d54df37a2275a7ffa73e5
Iteratum <emmaogbo2002@gmail.com>
2025-02-15 22:05:02 +0100

    Made some changes to the input components.

--- app/components/input.tsx
+++ app/components/input.tsx
@@ -16,18 +16,18 @@ const InputPassword: FC<InputPasswordProps> = ({ placeholder = "Enter your passw
     };
 
     return (
-        <div className="relative">
+        <div className="relative w-full md:w-[--input-width-md] h-[--input-height]">
             <input
                 type={isPasswordVisible ? "text" : "password"}
                 placeholder={placeholder}
                 value={value}
                 onChange={onChange}
-                className="w-screen md:w-[--input-width-md] h-[--input-height] pl-10 pr-4 py-2 outline-none border rounded-[--radius-sm]"
+                className="w-full h-full pl-10 pr-10 py-2 outline-none border rounded-[--radius-sm]"
             />
             <button
                 type="button"
                 onClick={togglePasswordVisibility}
-                className="absolute inset-y-0 right-0 px-4 py-2"
+                className="absolute inset-y-0 right-0 flex items-center px-4"
             >
                 <img
                     src={isPasswordVisible ? "/password-visibility-on.svg" : "/password-visibility-off.svg"}
@@ -54,13 +54,13 @@ interface InputEmailProps {
 
 const InputEmail: FC<InputEmailProps> = ({ placeholder = "Enter your email", value, onChange }) => {
     return (
-        <div className="relative">
+        <div className="relative w-full md:w-[--input-width-md] h-[--input-height]">
             <input
                 type="email"
                 placeholder={placeholder}
                 value={value}
                 onChange={onChange}
-                className="w-screen md:w-[--input-width-md] h-[--input-height] px-4 outline-none border rounded-[--radius-sm] pl-10"
+                className="w-full h-full pl-10 pr-4 py-2 outline-none border rounded-[--radius-sm]"
             />
             <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                 <img
@@ -81,12 +81,13 @@ interface InputPhoneNumberProps {
 
 const InputPhoneNumber: FC<InputPhoneNumberProps> = ({ placeholder = "Enter your phone number", value, onChange }) => {
     return (
-        <div className="relative">
+        <div className="relative w-full md:w-[--input-width-md] h-[--input-height]">
             <PhoneInput
                 placeholder={placeholder}
                 value={value}
                 onChange={onChange}
-                className="w-screen md:w-[--input-width-md] h-[--input-height] px-4 py-2 border outline-none rounded-[--radius-sm]"
+                defaultCountry="US"
+                className="w-full h-full px-4 py-2 border outline-none rounded-[--radius-sm]"
             />
         </div>
     );
--- app/routes/signup.tsx
+++ app/routes/signup.tsx
@@ -1,6 +1,17 @@
+import { useState } from "react";
+import { InputEmail, InputPassword, InputPhoneNumber } from "~/components/input";
+
 const SignUp = () => {
+    const [phoneNumber, setPhoneNumber] = useState("");
     return (
-        <div className="flex h-screen bg-[--primary]"></div>
+        <div className="h-screen bg-[--primary]">
+            <InputPassword />
+            <InputEmail />
+            <InputPhoneNumber
+                    value={phoneNumber}
+                    onChange={setPhoneNumber}
+                />
+        </div>
     )
 }
 
